57513244fa456d132b3e993e64631ac12bd20d5c
Carlos Ozuna <cozuna@Carloss-MacBook-Air.local>
2026-07-12 13:33:11 -0400

    feat: implement dynamic JSON-LD Schema for property pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.tucasard.com',
+  site: 'https://www.rodriguezjaviers.com',
   vite: {
     plugins: [tailwindcss()]
   },
